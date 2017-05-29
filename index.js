8d06ea1977fe16ba2a6f682658dbc0c9fc2779fc
Maik Hummel <yo@beevelop.com>
2017-05-29 16:35:19 +0200

    Compact and simplify code

--- index.js
+++ index.js
@@ -1,85 +1,33 @@
-var moment = require('moment')
-var winston = require('winston')
+'use strict'
+
+const moment = require('moment')
+const winston = require('winston')
 winston.remove(winston.transports.Console)
-winston.add(winston.transports.Console, {
-  'timestamp': true,
-  'colorize': true
-})
-
-var eventHandlers = {
-
-  /**
-   * On a new outage
-   * @param {Object} service
-   * @param {Object} outage
-   * @param {Object} outage.error check error
-   * @param {number} outage.timestamp outage timestamp
-   */
-  onNewOutage: (service, outage) => {
-    winston.error(`${service.name} down!`, outage.error)
-  },
-
-  /**
-   * Failed ping on an existing outage
-   * @param {Object} service
-   * @param {Object} outage
-   * @param {Object} outage.error check error
-   * @param {number} outage.timestamp outage timestamp
-   */
-  onCurrentOutage: (service, outage) => {
-    winston.error(`${service.name} is still down!`, outage.error)
-  },
-
-  /**
-   * Failed check (it will be an outage or not according to service.failuresToBeOutage
-   * @param {Object} service
-   * @param {Object} data
-   * @param {Object} data.error check error
-   * @param {number} data.currentFailureCount number of consecutive check failures
-   */
-  onFailedCheck: (service, data) => {
-    winston.error(`${service.name} check failed!`, data.error)
-  },
-
-  /**
-   * Warning alert
-   * @param {Object} service
-   * @param {Object} data
-   * @param {number} data.elapsedTime (ms)
-   */
-  onLatencyWarning: (service, data) => {
-    winston.warn(`${service.name} latency warning. Took: ${data.elapsedTime}ms`)
-  },
-
-  /**
-   * Service is back online
-   * @param {Object} service
-   * @param {Object} lastOutage
-   * @param {Object} lastOutage.error
-   * @param {number} lastOutage.timestamp (ms)
-   */
-  onServiceBack: (service, lastOutage) => {
-    var duration = moment.duration(+new Date() - lastOutage.timestamp, 'seconds')
-    winston.info(`${service.name} is back. Down for ${duration.humanize()}`)
-  },
-
-  /**
-   * Service is responding correctly
-   * @param {Object} service
-   * @param {Object} data
-   * @param {number} data.elapsedTime (ms)
-   */
-  onServiceOk: (service, data) => {
-    winston.info(`${service.name} responded OK! ${data.elapsedTime}ms`)
-  }
-}
+winston.add(winston.transports.Console, { 'timestamp': true, 'colorize': true })
 
 exports = module.exports = (watchmen) => {
-  watchmen.on('new-outage', eventHandlers.onNewOutage)
-  watchmen.on('current-outage', eventHandlers.onCurrentOutage)
-  watchmen.on('service-error', eventHandlers.onFailedCheck)
+  watchmen.on('new-outage', (service, outage) => {
+    winston.error(`${service.name} down!`, outage.error)
+  })
 
-  watchmen.on('latency-warning', eventHandlers.onLatencyWarning)
-  watchmen.on('service-back', eventHandlers.onServiceBack)
-  watchmen.on('service-ok', eventHandlers.onServiceOk)
+  watchmen.on('current-outage', (service, outage) => {
+    winston.error(`${service.name} is still down!`, outage.error)
+  })
+
+  watchmen.on('service-error', (service, data) => {
+    winston.error(`${service.name} check failed!`, data.error)
+  })
+
+  watchmen.on('latency-warning', (service, data) => {
+    winston.warn(`${service.name} latency warning. Took: ${data.elapsedTime}ms`)
+  })
+  
+  watchmen.on('service-back', (service, lastOutage) => {
+    let duration = moment.duration(moment().diff(lastOutage.timestamp))
+    winston.info(`${service.name} is back. Down for ${duration.humanize()}`)
+  })
+
+  watchmen.on('service-ok', (service, data) => {
+    winston.info(`${service.name} responded OK! ${data.elapsedTime}ms`)
+  })
 }
